a9e2524d0acfea47fe51731be2585b095f2eaf17
keithchin <kcisbug@gmail.com>
2021-01-23 18:21:09 +0800

    Update: Removed googleads component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,28 +1,28 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
 
 // styles
 const pageStyles = {
   color: "#232129",
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
+};
 
 const paragraphStyles = {
   marginBottom: 48,
-}
+};
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
   borderRadius: 4,
-}
+};
 
 // markup
 const IndexPage = () => {
@@ -35,20 +35,20 @@ const IndexPage = () => {
         <span role="img" aria-label="Pensive emoji">
           😔
         </span>{" "}
-        we couldn’t find what you were looking for. 
-        <br/>
+        we couldn’t find what you were looking for.
+        <br />
         {process.env.NODE_ENV === "development" ? (
           <>
-            <br/>
-              Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br/>
+            <br />
+            Try creating a page in <code style={codeStyles}>src/pages/</code>.
+            <br />
           </>
         ) : null}
-      <br/>
+        <br />
         <Link to="/">Go home</Link>.
       </p>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
